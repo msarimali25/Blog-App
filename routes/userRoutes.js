7cbaac4e397e8f535e0494d0e8005f49954e0e13
msarimali25 <msarimali2005@gmail.com>
2023-09-01 20:34:49 +0500

    first commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { getAllUsers, registerController, loginController } = require("../controllers/userController");
+
+//router object
+const router = express.Router();
+
+//Get All users || Get
+router.get("/all-users", getAllUsers );
+
+// CREATE USER || POST
+router.post("/register", registerController);
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+
+module.exports = router;
